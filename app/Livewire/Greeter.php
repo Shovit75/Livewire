dc1b05267314242782d38a418d83146fc08888f3
Shovit75 <shovittamang75@gmail.com>
2025-09-01 00:00:28 +0530

    Livewire Actions
    Actions to show a message based on toggleStatus and to toggle the language to Nepali or English.

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -6,7 +6,47 @@ use Livewire\Component;
 
 class Greeter extends Component
 {
-    public $hello = "Hello";
+    public $greeting = "Hello, this is from Livewire Component.";
+    public $hiddenMessage = "";
+    public $status = false;
+    public $toggleMessage = false;
+    public $spanishGreeting = "नमस्ते, यो Livewire Component बाट हो।";
+    public $englishGreeting = "Hello, this is from Livewire Component.";
+    public $name;
+    public $newName = 'Bubu Rani';
+
+    public function showHiddenMessage(){
+        if($this->status == true){
+            $this->hiddenMessage = "❤️ म तिमीलाई माया गर्छु, " . $this->newName . " !";
+        } else {
+            $this->hiddenMessage = "❤️ I love You, " . $this->newName . " !";
+        }
+        $this->toggleMessage = true;
+    }
+
+    public function toggleGreeting(){
+        if($this->status == false){
+            $this->greeting = $this->spanishGreeting;
+            if($this->toggleMessage == true){
+                $this->hiddenMessage = "❤️ म तिमीलाई माया गर्छु, " . $this->newName . " !";
+                $this->toggleMessage = true;
+            } else {
+                $this->hiddenMessage = "";
+                $this->toggleMessage = false;
+            }
+            $this->status = true;
+        } else {
+            $this->greeting = $this->englishGreeting;
+            if($this->toggleMessage == true){
+                $this->hiddenMessage = "❤️ I love You, " . $this->newName . " !";
+                $this->toggleMessage = true;
+            } else {
+                $this->hiddenMessage = "";
+                $this->toggleMessage = false;
+            }
+            $this->status = false;
+        }
+    }
 
     public function render()
     {
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,4 +1,19 @@
 <div>
     {{-- Be like water. --}}
-    <h1 class="mb-1 font-medium"> {{ $hello }}, this is from Livewire greeter Component.</h1>
+    <h1 class="mb-1 font-medium"> {{ $greeting }}</h1>
+
+    <input type="text" class="mt-2 mb-1 block w-full p-4 border rounded-md bg-gray-700 text-white" id="newName" wire:model.live="newName">
+
+    <button class="mt-2 mb-2 inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"
+        wire:click="toggleGreeting">
+        Toggle the languages.
+    </button>
+    
+    <button class="mt-2 mb-2 inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal"
+        wire:click="showHiddenMessage">
+        Show a hidden message.
+    </button>
+
+    <h1 class="mb-2 mt-2 font-medium"> {{ $hiddenMessage }} </h1>
+
 </div>
